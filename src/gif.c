7388756c32a560e5ee97800c1e55e3a59d03dd80
guojituo <guojituo1179@163.com>
2019-06-14 13:13:21 +0800

    backup.2019-06-14

--- src/gif.c
+++ src/gif.c
@@ -1,9 +1,189 @@
-﻿
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "gif.h"
 
+typedef FILE * GIF_HANDLE;
 
-
-void gifGet(void)
+static __inline GIF_HANDLE gifOpen(const char *fileName)
 {
+	GIF_HANDLE gifFp;
 
+	gifFp = fopen(fileName, "rb");
+	if(!gifFp) return 0;
 
+	return gifFp;
+}
+
+static __inline int gifClose(GIF_HANDLE gifFp)
+{
+	if(!gifFp) return -1;
+	fclose(gifFp);
+	return 0;
+}
+
+static __inline int gifRead(GIF_HANDLE gifFp, u8 *buf, u32 len)
+{
+	int ret;
+	if(!gifFp) return -1;
+	ret = fread(buf,1,len,gifFp);
+	return ret;
+}
+
+static __inline int gifWrite(GIF_HANDLE gifFp, const u8 *buf, u32 len)
+{
+	int ret;
+	if(!gifFp) return -1;
+	ret = fwrite(buf,1,len,gifFp);
+	return ret;
+}
+
+static int gifCalcFileSize(GIF_HANDLE gifFp)
+{
+	int p1,p2,size;
+
+	//计算文件大小
+	fseek(gifFp, 0, SEEK_SET);
+	p1=ftell(gifFp);
+	fseek(gifFp,0,SEEK_END);
+	p2=ftell(gifFp);
+	fseek(gifFp,0,SEEK_SET);
+	size = p2-p1;
+
+	return (size);
+}
+
+static int gifCheckHeader(u8 *headBuf, u8 headLen)
+{
+	if(strncmp((char*)headBuf, "GIF89a", GIF_HEADER_BYTES)==0 || \
+		strncmp((char*)headBuf, "GIF79a", GIF_HEADER_BYTES)==0) {
+		
+		return 1;
+	}
+
+	return 0;
+}
+
+int gifDecode(const unsigned char *fileName)
+{
+	GIF_HANDLE gifFp;
+	gifImage *gif;
+	int ret, i;
+	int ncolors;
+	u8 intro,label;
+
+	if(!fileName) return -1;
+	gif = (gifImage*)malloc(sizeof(gifImage));
+	if(!gif) {
+		printf("gif image malloc fail.\n");
+		return -1;
+	}
+
+	gifFp = gifOpen(fileName);
+	if(gifFp<=0) {
+		printf("gif file open fail.(%s)\n", fileName);
+	}
+
+	gif->size = gifCalcFileSize(gifFp);
+	printf("gif size: %d\n", gif->size);
+
+	ret = gifRead(gifFp, (u8*)&gif->header, GIF_HEADER_BYTES);
+	if(ret != GIF_HEADER_BYTES) {
+		printf("gif read header data fail.(%d)\n", ret);
+		goto ErrOut;
+	}
+
+	ret = gifCheckHeader((u8*)&gif->header, GIF_HEADER_BYTES);
+	if(!ret) goto ErrOut;
+
+	//Logical Screen Descriptor.
+	ret = gifRead(gifFp, (u8*)&gif->lsd, GIF_LSD_BYTES);
+	
+	printf("\nLogical Screen Descriptor:\n");
+	printf("width: %d\n", gif->lsd.width);
+	printf("height: %d\n", gif->lsd.height);
+	printf("flag: %d ## m(%d) cr(%d) s(%d) pixel(%d)\n", gif->lsd.flag, \
+		_LSD_M(gif->lsd.flag), \
+		_LSD_CR(gif->lsd.flag), \
+		_LSD_S(gif->lsd.flag), \
+		_LSD_PIXEL(gif->lsd.flag));
+	printf("bgIndex: %d\n", gif->lsd.bgIndex);
+	printf("pixelAspect: %d\n", gif->lsd.pixelAspect);
+	
+	//Global Color Table
+	if(_LSD_M(gif->lsd.flag)){
+		ncolors = 2<<(_LSD_PIXEL(gif->lsd.flag));	//颜色索引数=2^(pixel+1)
+		gif->gctSize = ncolors * 3;
+		gif->gct = (u8*)malloc(gif->gctSize);
+		if(!gif->gct) {
+			printf(" gif Global Color Table malloc fail.\n");
+			goto ErrOut;
+		}
+
+		ret = gifRead(gifFp, gif->gct, gif->gctSize);
+		if(ret != gif->gctSize) {
+			printf("get gobal color table fail.\n");
+			goto ErrOut;
+		}
+
+		printf("\nGlobal Color Table:\n");
+		printf("ColorTableSize: %d\n", gif->gctSize);
+		for(i=0; i<ncolors; i++){
+			printf("# %d #r:%d g:%d b:%d\n", i, gif->gct[i*3],gif->gct[i*3+1],gif->gct[i*3+2]);
+		}
+	}
+
+	do{		
+		//Image Descriptor	
+		ret = gifRead(gifFp, &intro, 1);
+		if(ret != 1) {
+			printf("intro read fail.\n");
+			goto ErrOut;
+		}
+		printf("intro:0x%x >> %d\n", intro);
+
+		switch(intro){
+		case GIF_INTRO_IMAGE:
+			ret = gifRead(gifFp, (u8*)&gif->isd, GIF_ISD_BYTES);
+			if(ret!=GIF_ISD_BYTES){
+				printf("read isd fail.\n");
+				goto ErrOut;
+			}
+			printf("\nImage Descriptor:\n");			
+			printf("xoffset: %d\n", gif->isd.xoffset);
+			printf("yoffset: %d\n", gif->isd.yoffset);
+			printf("width: %d\n", gif->isd.width);
+			printf("height: %d\n", gif->isd.height);
+			printf("flag: 0x%x m(%d) i(%d) s(%d) r(%d) pixel(%d)\n", gif->isd.flag,\
+				_ISD_M(gif->isd.flag),\
+				_ISD_I(gif->isd.flag),\
+				_ISD_S(gif->isd.flag),\
+				_ISD_R(gif->isd.flag),\
+				_ISD_PIXEL(gif->isd.flag));
+			ret = 0;
+			break;
+		
+		case GIF_INTRO_EXTENSION:
+			ret = gifRead(gifFp, (u8*)&label, 1);
+			if(ret!=1){
+				printf("read extension label fail.\n");
+				goto ErrOut;
+			}
+			printf("label:0x%x\n",label);
+
+			break;
+
+		case GIF_INTRO_TRAILER:
+			ret = 0;
+			break;
+		}
+
+	}while(ret!=0);
+
+ErrOut:
+	
+	if(gif && gif->gct) free(gif->gct);
+	if(gif) free(gif);
+	gifClose(gifFp);
+	return ret;
 }
--- src/gif.h
+++ src/gif.h
@@ -1,6 +1,90 @@
 ﻿#ifndef __GIF_H__
 #define __GIF_H__
 
+#define GIF_DATA_TYPE
 
+#if defined(GIF_DATA_TYPE)
+typedef signed char s8;
+typedef unsigned char u8;
+typedef signed short s16;
+typedef unsigned short u16;
+typedef signed int s32;
+typedef unsigned int u32;
+#endif
+
+
+#define GIF_HEADER_BYTES	(6)	//Signature and Version
+#define GIF_LSD_BYTES		(7)	//LogicalScreenDescriptor
+#define GIF_ISD_BYTES		(9)	//ImageDescriptor
+
+
+enum {
+	GIF_ERR_NONE = 0,
+	GIF_ERR_FORMAT
+};
+
+#define GIF_INTRO_TRAILER	0x3B	//GIF Trailer
+#define GIF_INTRO_EXTENSION	0x21	//'!'	Extension Introducer
+#define GIF_INTRO_IMAGE		0x2C	//','	Image Descriptor Introducer 
+
+// extension label
+#define _PLAIN_TEXT_LABEL				(0x01)	//Plain Text Label
+#define _GRAPHIC_CONTROL_LABEL			(0xf9)	//Graphic Control Label
+#define _COMMENT_LABEL					(0xfe)	//Comment Label
+#define _APPLICATION_EXTENSION_LABEL	(0xff)	//Application Extension Label
+
+
+#if defined(_WIN32)
+#pragma pack(1)
+#endif
+
+//逻辑屏幕描述块
+typedef struct{
+	u16 width;		//GIF宽度
+	u16 height;		//GIF高度
+#define _LSD_M(f)	  ((f>>7)&0x01)
+#define _LSD_CR(f)	  ((f>>4)&0x07)
+#define _LSD_S(f)	  ((f>>3)&0x01)
+#define _LSD_PIXEL(f) ((f)&0x07)
+	u8 flag;		//标识符[1:3:1:3] m[7]全局颜色表标志; cr[6:4]颜色深度; s[3]分类标志; pixel[2:0]全局颜色表大小
+	u8 bgIndex;		//背景色在全局颜色表中的索引(仅当存在全局颜色表时有效)
+	u8 pixelAspect;	//像素宽高比
+}gifLogicalScreenDescriptor;
+
+//图像描述块
+typedef struct {
+	u16 xoffset;	//X方向偏移
+	u16 yoffset;	//Y方向偏移
+	u16 width;		//宽度
+	u16 height;		//高度
+#define _ISD_M(f)		((f>>7)&0x01)
+#define _ISD_I(f)		((f>>6)&0x01)
+#define _ISD_S(f)		((f>>5)&0x01)
+#define _ISD_R(f)		((f>>3)&0x03)
+#define _ISD_PIXEL(f)	((f)&0x07)
+	u8 flag;		//标识符[1:1:1:2:3] m[7]局部颜色列表标志 i[6]交织标志 s[5]分类标志 r[4:3]保留，必须初始化为0 pixel[2:0]局部颜色列表大小
+}gifImageScreenDescriptor;
+
+typedef struct {
+	u32 size;	//file size
+	u8 header[GIF_HEADER_BYTES];	//Signature and Version
+	gifLogicalScreenDescriptor lsd;	//Logical Screen Descriptor
+	gifImageScreenDescriptor isd;	//Image Descriptor
+
+	//Global Color Table
+	u8 *gct;
+	u32 gctSize;
+
+	//Local Color Table
+	u8 *lct;
+	u32 lctSize;
+
+}gifImage;
+
+#if defined(_WIN32)
+#pragma pack()
+#endif
+
+int gifDecode(const unsigned char *fileName);
 
 #endif
